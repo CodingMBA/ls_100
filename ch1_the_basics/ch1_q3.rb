7d32971a78f397c019fa41ff9d54cfa847d59dc3
Andy Smith <andy@latelifedeveloper.com>
2017-09-24 16:40:08 +0000

    Adding solutions to Ch.1 The Basics exercises.

---
+++ ch1_the_basics/ch1_q3.rb
@@ -0,0 +1,12 @@
+movie_years = { :one_flew_over_the_cuckoo_nest => 1975,
+  :million_dollar_baby => 2004,
+  :twelve_years_a_slave => 2013,
+  :a_beautiful_mind => 2001,
+  :chariots_of_fire => 1981 }
+
+
+puts movie_years[:one_flew_over_the_cuckoo_nest]
+puts movie_years[:million_dollar_baby]
+puts movie_years[:twelve_years_a_slave]
+puts movie_years[:a_beautiful_mind]
+puts movie_years[:chariots_of_fire]
